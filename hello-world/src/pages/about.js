b8497ce45ac16d6027f06acacc7d1b977994c0a6
Marie Otaki <otakimarie@gmail.com>
2019-08-19 19:36:10 -0700

    commmit the gatsby official tutorial

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About"/>
+    <Header headerText="It's pretty good."/>
+    <p>Such wow. Very React.</p>
+  </div>
+)
